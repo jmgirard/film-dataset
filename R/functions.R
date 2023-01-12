89792df253b44f7c996d8c15965083d19502c906
jmgirard <me@jmgirard.com>
2023-01-12 08:05:08 -0600

    Rebuild

--- R/functions.R
+++ R/functions.R
@@ -1,14 +1,58 @@
 # Convert seconds to MM:SS format -----------------------------------------
 
-s_to_mmss <- function(s) {
+s_to_ts <- function(s, force_hours = FALSE, round_seconds = TRUE) {
+  
   p <- lubridate::seconds_to_period(s)
-  sprintf("%02d:%02d", lubridate::minute(p), lubridate::second(p))
+  h <- lubridate::hour(p)
+  m <- lubridate::minute(p)
+  s <- lubridate::second(p)
+  
+  if (any(h[is.finite(h)] > 0) || force_hours) {
+    if (round_seconds) {
+      out <- sprintf("%02d:%02d:%02d", h, m, round(s))
+    } else {
+      out <- sprintf("%02d:%02d:%02.3f", h, m, s)
+    }
+  } else {
+    if (round_seconds) {
+      out <- sprintf("%02d:%02d", m, round(s))
+    } else {
+      out <- sprintf("%02d:%02.3f", m, s)
+    }
+  }
+  
+  out[!is.finite(p)] <- NA_character_
+  
+  out
+}
+
+
+# Convert HH:MM:SS.sss format to seconds ----------------------------------
+
+ts_to_s <- function(ts) {
+  if (length(ts) > 1) {
+    map_dbl(ts, ts_to_s)
+  } else {
+    parts <- str_split(ts, pattern = ":", simplify = TRUE) |> parse_number()
+    parts[[1]]*60*60 + parts[[2]]*60 + parts[[3]]
+  }
 }
 
 # Get clip-specific information tibble ------------------------------------
 
 get_info_df <- function(abbrev) {
   df <- readxl::read_xlsx("./data/film_info.xlsx")
+  if (!missing(abbrev)) {
+    df <- df |> dplyr::filter(Abbrev == abbrev)
+  }
+  df
+}
+
+
+# Get mediainfo tibble ----------------------------------------------------
+
+get_mediainfo_df <- function(abbrev) {
+  df <- readxl::read_xlsx("./data/media_info.xlsx")
   if (!missing(abbrev)) {
     df <- df |> dplyr::filter(Abbrev == abbrev)
   }
@@ -33,6 +77,16 @@ get_valence_df <- function(abbrev) {
     readr::read_rds("./data/valence_ratings.rds") |>
     dplyr::rename(Timepoint = Second) |> 
     tidyr::drop_na(Rating)
+  if (!missing(abbrev)) {
+    df <- df |> dplyr::filter(Abbrev == abbrev)
+  }
+  df
+}
+
+# Get clip-specific subtitles tibble --------------------------------------
+
+get_subtitle_df <- function(abbrev) {
+  df <- readr::read_rds("./data/tidy_subtitles.rds")
   if (!missing(abbrev)) {
     df <- df |> dplyr::filter(Abbrev == abbrev)
   }
@@ -88,7 +142,7 @@ estimate_valence_icc_clip <- function(valence_df, info_df, iter = 10000) {
     rater = "Rater", 
     scores = "Rating",
     iter = iter,
-    file = paste0("data/icc_", info_df$Abbrev),
+    file = paste0("data/icc/icc_", info_df$Abbrev),
     silent = 2
   )
 }
@@ -134,7 +188,7 @@ create_valence_plot <- function(valence_df) {
     ggplot2::scale_x_continuous(
       breaks = seq(0, 10*2*30, 30), 
       expand = c(0, 0),
-      labels = s_to_mmss
+      labels = s_to_ts
     ) +
     ggplot2::scale_y_continuous(
       expand = c(0, 0)
@@ -146,5 +200,37 @@ create_valence_plot <- function(valence_df) {
       y = "Valence Rating", 
       x = "Timestamp within Clip"
     )
+}
+
+
+# Create clip-specific wordcloud ------------------------------------------
+
+create_wordcloud_clip <- function(subtitle_df) {
+  data("stop_words", package = "tidytext")
+  
+  subtitle_df |> 
+    dplyr::summarize(subtitle = stringr::str_c(subtitle, collapse = " ")) |>
+    dplyr::mutate(
+      subtitle = stringr::str_replace_all(subtitle, "\n", " "),
+      subtitle = stringr::str_remove_all(subtitle, "<i>"),
+      subtitle = stringr::str_remove_all(subtitle, "</i>"),
+      subtitle2 = textclean::replace_contraction(
+        subtitle, 
+        contraction.key = lexicon::key_contractions
+      ),
+      subtitle2 = textstem::lemmatize_strings(
+        subtitle2, 
+        dictionary = lexicon::hash_lemmas
+      )
+    ) |> 
+    tidytext::unnest_tokens("token", "subtitle2", token = "words") |> 
+    dplyr::count(token, sort = TRUE) |> 
+    dplyr::anti_join(stop_words, by = c("token" = "word")) |> 
+    ggplot2::ggplot(ggplot2::aes(label = token, size = n)) +
+    ggwordcloud::geom_text_wordcloud_area() +
+    ggplot2::scale_size_area(max_size = 18) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(plot.margin = ggplot2::margin(t = -2, l = 0, b = -2, r = 0))
+  
 }
 
